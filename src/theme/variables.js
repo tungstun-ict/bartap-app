48003c53867298cbb4a1e0bc622404547e099218
JortWillemsen <jort@maurice-nienke.nl>
2021-01-07 22:12:48 +0100

    Add grid for customers in the current session

--- src/theme/variables.js
+++ src/theme/variables.js
@@ -3,6 +3,8 @@ export const colors = {
   ELEMENT_BACKGROUND: "#212121",
   ELEMENT_BACKGROUND_SELECTED: "#323232",
 
+  ELEMENT_BACKGROUND_LIGHT: "#ffffff",
+
   TEXT_PRIMARY: "#fff",
   TEXT_SECONDARY: "#484848",
   TEXT_TERTIARY: "#383838",
@@ -15,7 +17,38 @@ export const sizes = {
 //TODO: Connect to API
 export const mock = {
   ORGANISATION_NAME: "Bart's Bar",
-  SESSION_NAME: "Oudjaarsavond",
+  CURRENT_SESSION_NAME: "Oudjaarsavond",
+
+  CURRENT_SESSION_CUSTOMERS: [
+    {
+      id: 1,
+      name: "Jort",
+      currentBill: {
+        total: 21.50,
+      }
+    },
+    {
+      id: 3,
+      name: "Jona",
+      currentBill: {
+        total: 26.50,
+      }
+    },
+    {
+      id: 5,
+      name: "Fee",
+      currentBill: {
+        total: 7.49,
+      }
+    },
+    {
+      id: 6,
+      name: "Dante",
+      currentBill: {
+        total: 2.10,
+      }
+    },
+  ],
 
   //TODO: Convert ID to key for every customer
   CUSTOMERS: [
