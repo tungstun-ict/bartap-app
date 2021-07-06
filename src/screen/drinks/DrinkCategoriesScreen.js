b00a1bf0f5aa8534e57512b8a7217bcf9aa115b5
Jort Willemsen <jort@maurice-nienke.nl>
2021-07-06 22:52:44 +0200

    Add light theme to drink screens

--- src/screen/drinks/DrinkCategoriesScreen.js
+++ src/screen/drinks/DrinkCategoriesScreen.js
@@ -3,12 +3,15 @@ import { RefreshControl, SafeAreaView } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
 import { Dimensions, FlatList } from "react-native";
 import { TouchableOpacity } from "react-native";
+import BarTapContent from "../../component/BarTapContent";
 
 import BarTapStackHeader from "../../component/BarTapStackHeader";
 import * as api from "../../service/BarApiService.js";
-import { darkTheme, mock } from "../../theme/variables.js";
+import { ThemeContext } from "../../theme/ThemeManager";
+import { theme, mock } from "../../theme/variables.js";
 
 export default function DrinkCategoriesScreen({ route, navigation }) {
+  const { theme } = React.useContext(ThemeContext);
   const [categories, setCategories] = useState([]);
   const [isLoading, setLoading] = useState(true);
 
@@ -52,11 +55,77 @@ export default function DrinkCategoriesScreen({ route, navigation }) {
     return data;
   };
 
+  const handleOnPress = (navigation, category, billId, sessionId) => {
+    navigation.navigate("Add Drink", {
+      category,
+      billId,
+      sessionId,
+    });
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "column",
+      backgroundColor: theme.BARTAP_BLACK,
+      alignItems: "center",
+      justifyContent: "flex-start",
+    },
+    content: {
+      flex: 1,
+      width: "100%",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 10,
+    },
+    itemInvisible: {
+      backgroundColor: "transparent",
+    },
+    category: {
+      flex: 1,
+      flexDirection: "column",
+      backgroundColor: theme.BARTAP_WHITE,
+      justifyContent: "center",
+      alignItems: "center",
+      marginVertical: 10,
+      maxHeight: Dimensions.get("window").height / 7,
+      maxWidth: Dimensions.get("window").width / 2 - 30,
+      minHeight: Dimensions.get("window").height / 7,
+      minWidth: Dimensions.get("window").width / 2 - 30,
+      borderRadius: 5,
+    },
+    categories__row: {
+      flex: 1,
+      justifyContent: "space-around",
+    },
+    category__name: {
+      textAlign: "center",
+      fontSize: 20,
+      fontWeight: "bold",
+      color: theme.BARTAP_BLACK,
+    },
+    categories: {
+      marginVertical: 10,
+      width: "100%",
+    },
+  });
+
+  const categoryListItem = (navigation, category, billId, sessionId) => {
+    return (
+      <TouchableOpacity
+        onPress={() => handleOnPress(navigation, category, billId, sessionId)}
+      >
+        <View style={styles.category}>
+          <Text style={styles.category__name}>{category.name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <BarTapStackHeader navigation={navigation} title="Add product" />
-      <View style={styles.content}>
-        <View style={styles.categories}>
+    <BarTapContent navigation={navigation} title={"Add product"}>
+      <View style={styles.categories}>
           <FlatList
             refreshControl={
               <RefreshControl onRefresh={() => setLoading(true)} refreshing={isLoading} tintColor="white" />
@@ -74,75 +143,6 @@ export default function DrinkCategoriesScreen({ route, navigation }) {
             columnWrapperStyle={styles.categories__row}
           />
         </View>
-      </View>
-    </SafeAreaView>
+    </BarTapContent>
   );
 }
-
-function categoryListItem(navigation, category, billId, sessionId) {
-  return (
-    <TouchableOpacity
-      onPress={() => handleOnPress(navigation, category, billId, sessionId)}
-    >
-      <View style={styles.category}>
-        <Text style={styles.category__name}>{category.name}</Text>
-      </View>
-    </TouchableOpacity>
-  );
-}
-
-function handleOnPress(navigation, category, billId, sessionId) {
-  navigation.navigate("Add Drink", {
-    category,
-    billId,
-    sessionId,
-  });
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: darkTheme.BARTAP_BLACK,
-    alignItems: "center",
-    justifyContent: "flex-start",
-  },
-  content: {
-    flex: 1,
-    width: "100%",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 10,
-  },
-  itemInvisible: {
-    backgroundColor: "transparent",
-  },
-  category: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: darkTheme.BARTAP_WHITE,
-    justifyContent: "center",
-    alignItems: "center",
-    marginVertical: 10,
-    maxHeight: Dimensions.get("window").height / 7,
-    maxWidth: Dimensions.get("window").width / 2 - 30,
-    minHeight: Dimensions.get("window").height / 7,
-    minWidth: Dimensions.get("window").width / 2 - 30,
-    borderRadius: 5,
-  },
-  categories__row: {
-    flex: 1,
-    justifyContent: "space-around",
-  },
-  category__name: {
-    textAlign: "center",
-    fontSize: 20,
-    fontWeight: "bold",
-    color: darkTheme.BARTAP_BLACK,
-  },
-  categories: {
-    marginVertical: 10,
-    width: "100%",
-  },
-});
